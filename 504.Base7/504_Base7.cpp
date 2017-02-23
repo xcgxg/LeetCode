3048e00000ce26e73e03396f2844d22ae5f827b7
xcgxg <ran987@126.com>
2017-02-23 17:06:12 +0000

    504

--- 504.Base7/504_Base7.cpp
+++ 504.Base7/504_Base7.cpp
@@ -2,23 +2,48 @@
 
 using namespace std;
 
+/*submission*/
+class Solution 
+{
+public:
+    string convertToBase7(int num) 
+    {
+        string res="";
+        int isNeg=num<0?1:0;
+        
+        if(isNeg)
+    	{
+    	    num=-num;
+    	}
+    
+        if(0 == num)
+        {
+            res="0";
+        }
+        else
+        {
+            while(0!=num)
+        	{
+        		res=(char)((num%7)+'0')+res;
+        		num/=7;
+        	}
+        	
+        	if(isNeg)
+        	{
+        	    res='-'+res;
+        	}
+        }
+
+    	return res;
+    }
+};
+/*submission*/
+
 int main()
 {
-	int a;
-	int b;
-	int count;
+	Solution s;
+	int num;
 
-	while(cin>>a)
-	{
-		count=b=0;
-
-		while(0!=a)
-		{
-			b+=(count*10+a%7);
-			a/=7;
-			count++;
-		}
-
-		cout<<b<<endl;
-	}
+	cin>>num;
+	cout<<s.convertToBase7(num)<<endl;
 }
